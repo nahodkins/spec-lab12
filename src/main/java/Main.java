3715e61ab25f3807502b180b5493f64bfe16226e
nahodkins <tsuber2001@gmail.com>
2021-12-11 18:34:12 +0200

    Adds first task implementation

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,22 @@
+import java.io.*;
+import java.net.URL;
+import java.net.URLConnection;
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        URL url = new URL("http://proglang.su/java/url-processing");
+        URLConnection urlConnection = url.openConnection();
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
+             BufferedWriter writer = new BufferedWriter(new FileWriter("output.html"))) {
+            reader.lines()
+                    .forEach(str -> {
+                        try {
+                            writer.write(str);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    });
+        }
     }
 }
