58db8d8e6666bc864bee7197bcae0d5d34bd36df
nahodkins <tsuber2001@gmail.com>
2021-12-17 00:40:50 +0200

    Do some refactors

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,15 @@
 import java.io.*;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-        URL url = new URL("http://proglang.su/java/url-processing");
+    private static void getHtmlPage(String pageUrl, String fileName) throws IOException {
+        URL url = new URL(pageUrl);
         URLConnection urlConnection = url.openConnection();
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
-             BufferedWriter writer = new BufferedWriter(new FileWriter("output.html"))) {
+             BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
             reader.lines()
                     .forEach(str -> {
                         try {
@@ -17,6 +18,13 @@ public class Main {
                             e.printStackTrace();
                         }
                     });
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+    }
+
+    public static void main(String[] args) throws IOException {
+        getHtmlPage("http://proglang.su/java/url-processing", "http-page.html");
+        getHtmlPage("https://github.com", "https-page.html");
     }
 }
